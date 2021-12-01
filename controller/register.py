9f74aa17085825ca26bb908e7d32b248df0ef4f5
Caio Reis <caiodtn@gmail.com>
2021-12-01 03:14:58 -0300

    controller to make interation with database

--- controller/register.py
+++ controller/register.py
@@ -8,14 +8,14 @@ class RegisterController:
     def __init__(self):
         self.db = Database()
 
+    # validate to check if username already exists in database
+    # hash password to insert in database
     def insert_user(self, username, password):
         user_db = self.db.get_user_data(data=username)
-        # print(username, password)
         if user_db:
             return None
         hashed_password = auth_handler.get_password_hash(password)
         result = self.db.insert_data('user', ('username', 'password'), (username, hashed_password))
-        print(result)
         if result:
             return result
 
