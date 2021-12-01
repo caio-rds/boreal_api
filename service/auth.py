39d564893523dd969c638adb48e0eb8c972ed68e
Caio Reis <caiodtn@gmail.com>
2021-12-01 03:09:33 -0300

    just comments and identation

--- service/auth.py
+++ service/auth.py
@@ -1,5 +1,4 @@
 from datetime import datetime, timedelta
-
 import jwt
 from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
@@ -11,20 +10,23 @@ class AuthHandler:
     pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
     secret = 'SECRET'
 
+    # return password as a hash
     def get_password_hash(self, password):
         return self.pwd_context.hash(password)
 
     def verify_password(self, plain_password, hashed_password):
         return self.pwd_context.verify(plain_password, hashed_password)
 
+    # encode token
     def encode_token(self, user_id):
         payload = {
-            'exp': datetime.utcnow() + timedelta(days=0, minutes=5),
+            'exp': datetime.utcnow() + timedelta(days=0, minutes=60),
             'iat': datetime.utcnow(),
             'sub': user_id
         }
         return jwt.encode(payload, self.secret, algorithm='HS256')
 
+    # decode token return username that belongs to token
     def decode_token(self, token):
         try:
             payload = jwt.decode(token, self.secret, algorithms=['HS256'])
